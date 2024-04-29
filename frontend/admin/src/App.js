2496c99a9119ed0d3e993d6d275af21e9c917c1f
NotKorah825 <2627971@students.wits.ac.za>
2024-04-29 13:49:09 +0200

    interface

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+function App(){
+    return (
+        <>
+            <main id="main" >
+                <header>
+                    <nav className="navigationbar">
+                        <section className="sec">
+                            <img src="business.png" alt="logo of the business" />
+                            <h1>The business Name</h1>
+                        </section>
+                        <img id="pp" src="pp.png" alt="user" />
+                        <i className="fa-solid fa-ellipsis-vertical" style={{ marginRight: '10px' }}></i>
+                    </nav>
+                </header>
+                <article className="postnav">
+                    <h2>The admin name</h2>
+                    <section className="sec1">
+                        <h3>Activities</h3>
+                    </section>
+                </article>
+                <article className="Cards">
+                    <section  className="Card">
+                        <article className="art">
+                            <h4>View Residents</h4>
+                            <i className="fa-solid fa-user-group"></i>
+                        </article>
+                      
+                    </section>
+                    <section className="Card">
+                        <article className="art">
+                            <h4>TICKETS</h4>
+                            <i className="fa-solid fa-ticket"></i>
+                        </article>
+                       <total />
+                    </section>
+                    <section  className="Card">
+                        <article className="art">
+                            <h4>View STAFF</h4>
+                            <i className="fa-solid fa-user-helmet-safety"></i>
+                        </article>
+                      
+                    </section>
+                </article>
+                <article id="viewBox" className='viewBox'>
+                    <section id="header" className="header">
+                
+                    </section>
+                    <section id="viewComp" className="viewComp">
+      
+                    </section>
+                </article>
+            </main>
+  
+           
+        </>
+    );
 }
 
 export default App;
